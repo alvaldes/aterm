3d72d69bb0cea693138d013df87ff9777581526a
alvaldes <angelluis2605@gmail.com>
2022-11-24 03:51:37 -0500

    Add version 1.0 of aterm

---
+++ js/commands.js
@@ -0,0 +1,82 @@
+let twitter = 'http://www.twitter.com/alvaldes_';
+let linkedin = 'https://www.linkedin.com/in/forrestpknight/';
+let github = 'https://github.com/forrestknight/';
+let email = 'mailto:angelluis2605@gmail.com';
+let website = 'https://www.alvaldes.vercel.app/';
+let existCommands = [
+  'about',
+  'banner',
+  'clear',
+  'cls',
+  'email',
+  'github',
+  'gui',
+  'help',
+  'history',
+  'linkedin',
+  'projects',
+  'repo',
+  'social',
+  'theme',
+  'twitter',
+  'weather',
+];
+
+about = ['<br>', "Hello, I'm Angel Valdés👋", '<br>'];
+
+social = [
+  '<br>',
+  'twitter           <a href="' +
+    twitter +
+    '" target="_blank">twitter/alvaldes_</a>',
+  'linkedin          <a href="' +
+    linkedin +
+    '" target="_blank">linkedin/alvaldes</a>',
+  'github            <a href="' +
+    github +
+    '" target="_blank">github/alvaldes</a>',
+  '<br>',
+];
+
+projects = ['<br>', 'On Progress ......', '<br>'];
+
+help = [
+  '<br>',
+  '<span class="command">about</span>              Who is alvaldes?',
+  '<span class="command">banner</span>             Display the header',
+  '<span class="command">clear | cls</span>        Clear the teminal',
+  '<span class="command">email</span>              Send me an email',
+  '<span class="command">gui</span>                Open the Portfolio Website',
+  '<span class="command">help</span>               Check available commands',
+  '<span class="command">history</span>            View command history',
+  '<span class="command">projects</span>           View coding projects',
+  '<span class="command">repo</span>               Open the source code',
+  '<span class="command">social</span>             Display social networks',
+  '<span class="command">theme</span>              Theme configurations',
+  '<span class="command">weather</span>            Display the weather of a [city]',
+  '<br>',
+  '[TAB]              Trigger completion',
+  '[Ctrl+l]           Clear terminal',
+  '<br>',
+];
+
+banner = [
+  '<span class="index">All up to date | aterm v1.0. </span>',
+  '              .          .              ,                ,    ,          .                 .',
+  '.        &bsol;          .                  __       .           *            ,',
+  '   .      &bsol;   ,                       /&bsol; &bsol;__',
+  '.          o     .            .__     &bsol; &bsol; ,_&bsol;     __    _ __    ___ ___.            .',
+  '  .         &bsol;            ,   /&#39;__`&bsol;    &bsol; &bsol; &bsol;/   /&#39;__`&bsol; /&bsol;`&#39;__&bsol;/&#39; __` _` &bsol;       .',
+  '            #&bsol;##&bsol;#    .     /&bsol; &bsol;L&bsol;.&bsol;_   &bsol; &bsol; &bsol;_ /&bsol;  __/ &bsol; &bsol; &bsol;/ /&bsol; &bsol;/&bsol; &bsol;/&bsol; &bsol;    .        .',
+  '          #  #O##&bsol;###     . &bsol; &bsol;__/.&bsol;_&bsol;   &bsol; &bsol;__&bsol;&bsol; &bsol;____&bsol; &bsol; &bsol;_&bsol; &bsol; &bsol;_&bsol; &bsol;_&bsol; &bsol;_&bsol;            .',
+  '.        #*#  #&bsol;##&bsol;###       &bsol;/__/&bsol;/_/    &bsol;/__/ &bsol;/____/  &bsol;/_/  &bsol;/_/&bsol;/_/&bsol;/_/     .       ,',
+  '     .   ##*#  #&bsol;##&bsol;##               .                     .          ,                   .        ,',
+  '   .      ##*#  #o##&bsol;#         .                             ,       .       ,               .   ',
+  '       .     *#  #&bsol;#     .                    .             .          ,                .    ',
+  '                   &bsol;          .                         .           .             ,       .',
+  '____^/___^--____/___O______________//---/___________---__/____O______//---/________',
+  "/^   ^  ^    ^                  ^^ ^  ' ^          ^       --^    ^                  ^^ ^  ' ^ -",
+  '--  __      --  -      -  ---  --  ____--  ^  ^  -  --  __    ___--  ^___@alvaldes --  __',
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+];
---
+++ js/main.js
@@ -0,0 +1,174 @@
+let before = document.getElementById('before');
+let liner = document.getElementById('liner');
+let command = document.getElementById('typer');
+let textarea = document.getElementById('texter');
+let terminal = document.getElementById('terminal');
+
+let commands = [];
+let git = 0;
+
+setTimeout(function () {
+  loopLines(banner, '', 80);
+  textarea.focus();
+}, 100);
+
+window.addEventListener('keyup', enterKey);
+window.addEventListener('keydown', keyDown);
+
+textarea.value = '';
+command.innerHTML = textarea.value;
+
+function keyDown(e) {
+  if (e.ctrlKey && e.key == 'l') {
+    e.preventDefault();
+    cls();
+  }
+  if (e.key === 'Tab') {
+    e.preventDefault();
+    autoComplete();
+  }
+}
+
+function enterKey(e) {
+  if (e.keyCode == 13) {
+    commands.push(command.innerHTML);
+    git = commands.length;
+    addLine('guest@aterm.com:~$ ' + command.innerHTML, 'no-animation', 0);
+    commander(command.innerHTML.toLowerCase());
+    command.innerHTML = '';
+    textarea.value = '';
+  }
+  if (e.keyCode == 38 && git != 0) {
+    git -= 1;
+    textarea.value = commands[git];
+    command.innerHTML = textarea.value;
+  }
+  if (e.keyCode == 40 && git != commands.length) {
+    git += 1;
+    if (commands[git] === undefined) {
+      textarea.value = '';
+    } else {
+      textarea.value = commands[git];
+    }
+    command.innerHTML = textarea.value;
+  }
+}
+
+function commander(cmd) {
+  switch (cmd) {
+    case 'about':
+      loopLines(about, 'color margin', 80);
+      break;
+    case 'banner':
+      loopLines(banner, '', 80);
+      break;
+    case 'clear':
+      cls();
+      break;
+    case 'cls':
+      cls();
+      break;
+    case 'email':
+      addLine(
+        'Opening mailto:<a href="mailto:angelluis2605@gmail.com">angelluis2605@gmail.com</a>...',
+        'color2',
+        80
+      );
+      newTab(email);
+      break;
+    case 'gui':
+      addLine('Opening GUI version...', 'color2', 80);
+      newTab(website);
+      break;
+    case 'help':
+      loopLines(help, 'color margin', 80);
+      break;
+    case 'history':
+      addLine('<br>', '', 0);
+      loopLines(commands, 'color2', 80);
+      addLine('<br>', 'command', 80 * commands.length + 50);
+      break;
+    case 'projects':
+      loopLines(projects, 'color margin', 80);
+      break;
+    case 'repo':
+      loopLines(help, 'color margin', 80);
+      break;
+    case 'social':
+      loopLines(social, 'color margin', 80);
+      break;
+    case 'twitter':
+      addLine('Opening Twitter...', 'color2', 0);
+      newTab(twitter);
+      break;
+    case 'linkedin':
+      addLine('Opening LinkedIn...', 'color2', 0);
+      newTab(linkedin);
+      break;
+    case 'github':
+      addLine('Opening GitHub...', 'color2', 0);
+      newTab(github);
+      break;
+    case 'theme':
+      loopLines(help, 'color margin', 80);
+      break;
+    case 'weather':
+      loopLines(help, 'color margin', 80);
+      break;
+    default:
+      addLine(
+        '<span class="inherit">Command not found. For a list of commands, type <span class="command">\'help\'</span>.</span>',
+        'error',
+        100
+      );
+      break;
+  }
+}
+
+function newTab(link) {
+  setTimeout(function () {
+    window.open(link, '_blank');
+  }, 500);
+}
+
+function addLine(text, style, time) {
+  let t = '';
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == ' ' && text.charAt(i + 1) == ' ') {
+      t += '&nbsp;&nbsp;';
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(function () {
+    let next = document.createElement('p');
+    next.innerHTML = t;
+    next.className = style;
+
+    before.parentNode.insertBefore(next, before);
+
+    window.scrollTo(0, document.body.offsetHeight);
+  }, time);
+}
+
+function loopLines(name, style, timeout) {
+  name.forEach((item, index) => {
+    addLine(item, style, index * timeout);
+  });
+}
+
+function cls() {
+  setTimeout(function () {
+    terminal.innerHTML = '<a id="before"></a>';
+    before = document.getElementById('before');
+  }, 1);
+}
+
+function autoComplete() {
+  let arr = existCommands.filter((item) => item.startsWith(command.innerHTML));
+  if (arr.length === 1) {
+    textarea.value = arr[0];
+    command.innerHTML = arr[0];
+  }
+}
